e2dfb88eb9f2c5e7b84fe39e2d4fb84e05cad3f7
Michael Koenig <koenigmichael2002@gmail.com>
2024-01-25 18:25:53 +0100

    Add slices package

---
+++ slices/sum.go
@@ -0,0 +1,31 @@
+package main
+
+func Sum(numbers []int) (sum int) {
+
+	for _, number := range numbers {
+		sum += number
+	}
+	return
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	sums := make([]int, len(numbersToSum))
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return sums
+}
